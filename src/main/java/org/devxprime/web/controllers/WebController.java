30dacf76edf8c0321c83767ab313bdf5b806474f
kanchan-kumar <kanchan-kumar@live.com>
2018-12-25 22:24:18 +0530

    adding api

--- src/main/java/org/devxprime/web/controllers/WebController.java
+++ src/main/java/org/devxprime/web/controllers/WebController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -16,5 +17,10 @@ public class WebController {
 	@RequestMapping(value="/home")
 	public ResponseEntity<Object> home() {
 		return new ResponseEntity<>("Hello from Spring App", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/post", method = RequestMethod.POST)
+	public ResponseEntity<Object> postReq() {
+		return new ResponseEntity<>("Hello from Spring App Post data", HttpStatus.OK);
 	}
 }
