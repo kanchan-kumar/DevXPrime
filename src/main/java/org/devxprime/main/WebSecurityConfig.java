1fae305851c287c130c45321f64290cda29345f9
kanchan-kumar <kanchan-kumar@live.com>
2019-09-14 18:59:15 +0530

    bug fixes

--- src/main/java/org/devxprime/main/WebSecurityConfig.java
+++ src/main/java/org/devxprime/main/WebSecurityConfig.java
@@ -16,6 +16,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.cors().and().csrf().disable();
+        //http.requestMatchers().antMatchers("/users/all").and().authorizeRequests();
+        http.authorizeRequests().antMatchers("/").permitAll();
     }
 
     @Bean
