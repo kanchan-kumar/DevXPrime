3de82a94c3c45610394364e2baef3fce13501a53
kanchan-kumar <kanchan-kumar@live.com>
2019-10-12 19:53:02 +0530

    v1

--- src/main/java/org/devxprime/main/DevToolKitXPrimeApplication.java
+++ src/main/java/org/devxprime/main/DevToolKitXPrimeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages="org.devxprime.web.controllers, org.devxprime.web.exception, "
 		+ "org.devxprime.web.services, org.devxprime.web.props, org.devxprime.utils, org.devxprime.tools.decompilers")
 @EnableConfigurationProperties(StorageProperties.class)
-public class DevToolKitXPrimeApplication {
+public class DevToolKitXPrimeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevToolKitXPrimeApplication.class, args);
